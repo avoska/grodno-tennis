15b5144c7e6803b44fdc09aba7bc7026681bb330
Yan-235 <34718057+Yan-235@users.noreply.github.com>
2018-04-06 00:48:42 +0300

    #7 Failed building db. Unknown error in migrations

--- app/User.php
+++ app/User.php
@@ -3,27 +3,36 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
+class User extends Model {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	use Notifiable;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'lastname', 'name', 'secondname', 'age', 'matches', 'wins', 'email', 'password',
+	];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token', 'role'
+	];
+
+	public function matches_users() {
+		return $this->belongsToMany('App\Match_has_user', 'match-user', 'id', 'user_id');
+	}
+
+	public function users_tournaments() {
+		return $this->belongsToMany('App\Tournament_has_user', 'tournament-user', 'id', 'user_id');
+	}
 }
